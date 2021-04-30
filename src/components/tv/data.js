41f5c48a521ac73504101b029e124e281cbaf230
stresslimit <colin.vernon@gmail.com>
2021-04-30 15:43:56 -0400

    update video list

--- src/components/tv/data.js
+++ src/components/tv/data.js
@@ -1,99 +1,13 @@
-
-export default [
-  {
-    id: 'rvpkRXp6qbc',
-    start: ''
-  },
-  {
-    id: 'tN8RE02iNII',
-    start: ''
-  },
-  {
-    id: 'GvBX2WsXEo0',
-    start: ''
-  },
-  {
-    id: 'TPkXAi_IKwQ',
-    start: ''
-  },
-  {
-    id: 'thYg0GP8qsM',
-    start: ''
-  },
-  {
-    id: '_dWJVHIE9S8',
-    start: ''
-  },
-  {
-    id: 'ylkWpE89nn8',
-    start: ''
-  },
-  {
-    id: 'yC9wZTJmDqA',
-    start: ''
-  },
-  {
-    id: '0SdenMzFgBg',
-    start: ''
-  },
-  {
-    id: 'WxXy9lsiUPM',
-    start: ''
-  },
-  {
-    id: 'J6J9EfOdPgg',
-    start: ''
-  },
-  {
-    id: 'UpIMFker8Mg',
-    start: ''
-  },
-  {
-    id: '2m8YACFJlMg',
-    start: ''
-  },
-  {
-    id: 'w2-e8eERg_c',
-    start: ''
-  },
-  {
-    id: 't513R-oEn0g',
-    start: '275'
-  },
-  {
-    id: 'mC84VMYUzoI',
-    start: ''
-  },
-  {
-    id: 'P4q4rspgtD4',
-    start: ''
-  },
-  {
-    id: 'mc_IgE7TBSY',
-    start: ''
-  },
-  {
-    id: 'AOhQZlNptJY',
-    start: ''
-  },
-  {
-    id: 'NmRgQcMPlQo',
-    start: ''
-  },
-  {
-    id: 'd-9hmEiH828',
-    start: ''
-  },
-  {
-    id: 'w90mHA-S9NY',
-    start: ''
-  },
-  {
-    id: 'UD9PDfLpL1c',
-    start: ''
-  },
-  {
-    id: 'YJSehRlU34w',
-    start: ''
-  },
+export default [ { id: 'pQaZrS4STdA', start: '' },
+  { id: 'rvpkRXp6qbc', start: '' },
+  { id: '_dWJVHIE9S8', start: '' },
+  { id: 'UpIMFker8Mg', start: '' },
+  { id: '2m8YACFJlMg', start: '' },
+  { id: 'w2-e8eERg_c', start: '' },
+  { id: 'P4q4rspgtD4', start: '' },
+  { id: 'AOhQZlNptJY', start: '' },
+  { id: 'NmRgQcMPlQo', start: '' },
+  { id: 'YJSehRlU34w', start: '' },
 ];
+
+// THIS FILE IS FORMATTED LIKE THIS TO MATCH LINE NUMBERS TO VIDEO UI
